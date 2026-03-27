2806d0eefe6bb00ccfee979d3a6c0686e58ec76f
raaywasdead <joaov.bds20@gmail.com>
2026-03-27 19:32:26 -0300

    fix: importar useEffect no ContactPage

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import { useIsMobile } from '../hooks/useIsMobile'
 import { motion } from 'framer-motion'
 
